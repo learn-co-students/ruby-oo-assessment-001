644a107183bd8595bc2c9edd33fe3cb1f8db2e51
maiya777 <maiya777@list.ru>
2015-11-24 22:08:51 +0300

    Done

--- lib/mark_odds.rb
+++ lib/mark_odds.rb
@@ -1,2 +1,10 @@
 # Modify the Array class to support an odds? method that
 # returns all the odd integers in the array.
+
+class Array
+
+def odds?
+  self.map {|int| int.odd? ? int = true : int = false }
+end
+
+end
--- lib/meal.rb
+++ lib/meal.rb
@@ -2,3 +2,13 @@
 
 # Meal should accept an optional initialization argument that specifies
 # the meal choice. The default value for that argument should be meat.
+
+class Meal
+  attr_accessor :choice
+
+def initialize(meal_choice="meat")
+  @choice = meal_choice
+end
+
+
+end
