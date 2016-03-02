830314b184104c0e5ed2c9a45940f1860b10735b
thaik1618 <kthai1618@gmail.com>
2016-03-02 01:22:01 +0100

    Done.

--- spec/015_path_spec.rb
+++ spec/015_path_spec.rb
@@ -24,3 +24,29 @@ require_relative "../lib/path.rb" # Code your solution here.
 # Test the negative and positive results, for example,
 # when describing absolute_path? make sure to test
 # the results when sent a relative and absolute path.
+
+describe Path do
+
+  let(:path) { Path.new("/ruby-oo-assessment-001/lib/dog.rb") }
+  let(:rel_path) { Path.new("ruby-oo-assessment-001/lib/dog.rb") }
+
+  describe "#normalize_path" do
+    it "should return an absolute path to the file" do
+      expect(path.absolute_path?).to eq(true)
+    end
+  end
+
+  describe "#relative_path?" do
+    it "should return true if the path passed to it is relative" do
+      expect(path.relative_path?).to eq(false)
+      expect(rel_path.relative_path?).to eq(true)
+    end
+  end
+
+  describe "#absolute_path?" do
+    it "should return true if the path passed to it is absolute" do
+      expect(path.absolute_path?).to eq(true)
+      expect(rel_path.absolute_path?).to eq(false)
+    end
+  end
+end
